55de76ebc9b8581338a80ede7a1180186891503a
tudptse17520 <tudptse17520@gmail.com>
2026-02-04 22:56:58 +0700

    Revert "update app mobile"
    This reverts commit 8f1eca0d685cf28e59d9795bccdb792dba75d9f6.

--- mobile-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "com.anhhai.restaurant",
-  appName: "anh-hai-restaurant",
-  webDir: "dist",
-};
-
-export default config;
